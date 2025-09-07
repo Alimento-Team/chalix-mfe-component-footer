bcf8936c5ad18d8005b32259b7ba35aee0683af0
dungdl <dungdl0298@gmail.com>
2025-09-07 15:41:31 +0700

    Add built dist files for release

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import Footer, { EVENT_NAMES } from './components/Footer';
+import ChalixFooter from './components/ChalixFooter';
+import messages from './i18n/index';
+import StudioFooter from './components/studio-footer';
+import FooterSlot from './plugin-slots/FooterSlot';
+import StudioFooterSlot from './plugin-slots/StudioFooterSlot';
+export default Footer;
+export { ChalixFooter, messages, EVENT_NAMES, StudioFooter, FooterSlot, StudioFooterSlot };
+//# sourceMappingURL=index.js.map
